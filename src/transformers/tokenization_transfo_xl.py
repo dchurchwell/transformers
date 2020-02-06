db1735d916e10c1a479f602512409a12bf623a80
dchurchwell <damienchurchwell@gmail.com>
2020-02-06 01:51:13 -0700

    Changed vocabulary save function. Variable name was inconsistent, causing an error to be thrown when passing a file name instead of a directory.

--- src/transformers/tokenization_transfo_xl.py
+++ src/transformers/tokenization_transfo_xl.py
@@ -158,9 +158,9 @@ class TransfoXLTokenizer(PreTrainedTokenizer):
     def save_vocabulary(self, vocab_path):
         """Save the tokenizer vocabulary to a directory or file."""
         if os.path.isdir(vocab_path):
-            vocab_file = os.path.join(vocab_path, VOCAB_FILES_NAMES["pretrained_vocab_file"])
-        torch.save(self.__dict__, vocab_file)
-        return (vocab_file,)
+            vocab_path = os.path.join(vocab_path, VOCAB_FILES_NAMES["pretrained_vocab_file"])
+        torch.save(self.__dict__, vocab_path)
+        return (vocab_path,)
 
     def build_vocab(self):
         if self.vocab_file:
